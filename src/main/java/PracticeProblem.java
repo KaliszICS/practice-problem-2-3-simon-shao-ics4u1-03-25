d8b2e3096d6f6e83386664dcc78d238c1fe152ef
simon-shao-ics4u1-03-25 <simon.shao@student.tdsb.on.ca>
2025-03-20 18:53:13 +0000

    Complete Assignment

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,27 +1,33 @@
 public class PracticeProblem {
 
 	public static void main(String args[]) {
-
+		String[] word = new  String[] {"a", "b", "D", "c", "help", "me", "you", "fool"};
+bubbleSortString(word);
+for (int i = 0; i<word.length;i++){
+	System.out.println(word[i]);
+}
 	}
 
-	public static void q1() {
-		//Write question 1 code here
+	public static void bubbleSortString(String[] strings){
+		boolean swapped = true;
+String bucket = "";
+		for(int i = 0; i<strings.length-1 && swapped;i++){
+			swapped = false;
+			for(int s = 0; s<strings.length-1-i;s++){
+				if (strings[s].compareToIgnoreCase(strings[s+1]) >0){
+					bucket = strings[s+1];
+					strings[s+1] = strings[s];
+					strings[s] = bucket;
+					swapped = true;
+					System.out.println(strings[s]);
+				}
 	}
+}
 
-	public static void q2() {
-		//Write question 2 code here
-	}
 
-	public static void q3() {
-		//Write question 3 code here
-	}
 
-	public static void q4() {
-		//Write question 4 code here
-	}
 
-	public static void q5() {
-		//Write question 5 code here
+
 	}
 
 }
